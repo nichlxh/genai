fe38c0c95f0d0382b51ee1c89f648fe2e98e7802
nichlxh <nichlxh@live.com>
2024-11-03 13:51:48 +0800

    ok

--- streamlitCisco_v1_github.py
+++ streamlitCisco_v1_github.py
@@ -76,11 +76,11 @@ elif modelType == 'langchain_OpenAI':
     llm = ChatOpenAI(model="gpt-4o-mini")
 
     import bs4
-    from langchain import hub
-    from langchain_chroma import Chroma
+    # from langchain import hub
+    # from langchain_chroma import Chroma
     from langchain_community.document_loaders import WebBaseLoader
-    from langchain_core.output_parsers import StrOutputParser
-    from langchain_core.runnables import RunnablePassthrough
+    # from langchain_core.output_parsers import StrOutputParser
+    # from langchain_core.runnables import RunnablePassthrough
     from langchain_openai import OpenAIEmbeddings
     from langchain_text_splitters import RecursiveCharacterTextSplitter
 
